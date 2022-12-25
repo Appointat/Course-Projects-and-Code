80697926ef030a30e591f90b80fa4305453f268c
Zikang CHEN <appointat@shu.edu.cn>
2022-12-26 00:43:45 +0100

    Update SR01 Processus et Programmation Système

---
+++ SR01/Processus et Programmation Système/Création de Processus avec fork().cpp
@@ -0,0 +1,27 @@
+#include <sys/types.h> /* Types pid_t... */
+#include <unistd.h> /* fork()... */
+#include <stdio.h> /* printf... */
+#include <stdlib.h> /* EXIT_FAILURE... */
+#define DURATION 15
+int main()
+{
+	pid_t pid_fils; int i;
+	switch (pid_fils = fork()) {
+	case (pid_t)-1:
+		/* Retour du fork() = -1 => echec (eg. manque de ressources) */
+		perror("main/fork");
+		return EXIT_FAILURE;
+	case (pid_t)0: /* Retour du fork() = 0 => processus fils */
+		for (i = DURATION; i > 0; i--) {
+			printf("Fils : je suis vivant pour encore %d secondes.\n", i);
+			fflush(stdout); sleep(1);
+		}
+		return EXIT_SUCCESS;
+	default: /* Retour du fork() != 0 donc c¡¯est le pere */
+		for (i = DURATION; i > 0; i--) {
+			printf("Pere : je suis vivant pour encore %d secondes.\n", i);
+			fflush(stdout); sleep(1);
+		}
+	}
+	return EXIT_SUCCESS;
+}
